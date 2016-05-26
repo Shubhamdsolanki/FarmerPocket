50c872b5066124d0b342de90ffde27a742275ba1
breewf <haiygao@163.com>
2016-05-26 10:17:04 +0800

    帮助中心
    帮助中心

--- app/src/main/java/com/company/farmerpocket/activity/HelpActivity.java
+++ app/src/main/java/com/company/farmerpocket/activity/HelpActivity.java
@@ -18,6 +18,11 @@ public class HelpActivity extends AbsBaseActivity {
     }
 
     @Override
+    protected boolean isOpenSwipeBack() {
+        return true;
+    }
+
+    @Override
     protected void init() {
 
     }
